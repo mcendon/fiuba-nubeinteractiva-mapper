05a2dcc192b50526dedb76ce1326e22401d64266
mau.cendon@gmail.com <mau.cendon@gmail.com@5af687ca-4c63-ab43-ae9e-5406cb19572c>
2013-10-07 06:40:47 +0000

    # Se sacaron los routes que estaban y se unificaron en uno que toma: <redsocial>,<evento>,<numero> # Se agregó soporte multi-threading # Se crearon 3 threads: Controlador, Mapper y Rest (No consegui hacer que el Rest corra en el main x eso lo separé) # Se creo una funcionalidad basica que recibe un numero de notificaciones, lo acumula. El mapper va preguntando al controlador si esta disponible (se puso un random 1 o 0 en el controlador) y va despachando las notificaciones.

--- mapper/mapper.py
+++ mapper/mapper.py
@@ -1,50 +1,79 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
-app = Flask(__name__)
+import threading, time, signal, sys, random
 
-app.config.update(dict(
-    DEBUG=True, #Codear y debuggear sin necesidad de resetear la app
-))
+#Controlador
+#Controlador del arduino
+class Controlador():
+	def isIdle(self):
+		return bool(random.randint(0,1))
+		
+	def hacerEfecto(self,idEfecto):
+		print("[Mensaje del Controlador]: Despache un efecto a la nube")
+		
+#TODO: Cambiar esto por un map para contar notificaciones x red social
+#Onda {''facebook': 5, 'twitter': 10}
+acum = 0  #acumulacion
 
-#Index de la app (http://localhost:5000)
-@app.route('/')
-def index():
-	url_for('static', filename='nube_mapper.jpg');
-	return render_template('index.html');
 
-@app.route('/facebook/<event>', methods=['GET', 'POST'])
-def facebook(event):
-	#TODO: Implementar logica de cada evento
-	if event == 'like': 
-		facebook_like();
-		return 'true';
-	if event == 'comment':
-		facebook_comment();
-		return 'true';
-	return 'false';
-	
-def facebook_like():
-	return;
-	
-def facebook_comment():
-	return;
-	
-@app.route('/twitter/<event>', methods=['GET', 'POST'])
-def twitter(event):
-	#TODO: Implementar logica de cada evento
-	if event == 'hashtag': 
-		twitter_hashtag();
-		return 'true';
-	if event == 'mention':
-		twitter_mention();
-		return 'true';
-	return 'false';
-	
-def twitter_hashtag():
-	return;
+#Mapper
+#Thread que esta corriendo continuamente y mapea acciones al Controlador
+class Mapper(threading.Thread):  
+	def __init__(self):  
+		threading.Thread.__init__(self)  
+		self.controlador = Controlador()
 
-def twitter_mention():
-	return;
-	
+	def run(self):
+		global acum
+		while(True):
+			if acum > 0:
+				print "[Mensaje del Mapper]: Tengo ", acum, " notificaciones para procesar"
+				if not self.controlador.isIdle():
+					print "[Mensaje del Mapper]: El controlador ESTA disponible :)"
+					acum = acum - 1
+					self.controlador.hacerEfecto(400)
+				else:
+					print "[Mensaje del Mapper]: El controlador no esta disponible :("
+			else:
+				print "[Mensaje del Mapper]: Durmiendo... zzzzzZzzZzz"
+			time.sleep(1)
+
+#Rest
+#Servidor Rest implementado con Flask
+#No consegui (Mauro) hacer que corra en el hilo principal de forma correcta junto con el Mapper
+#Tuve que ponerlo en otro thread separado
+class Rest(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		
+	def run(self):
+		app = Flask(__name__)
+		
+		app.config.update(dict(
+			DEBUG=False, #Codear y debuggear sin necesidad de resetear la app
+		))
+		
+		#Index de la app (http://localhost:5000)
+		@app.route('/')
+		def index():
+			url_for('static', filename='nube_mapper.jpg');
+			return render_template('index.html');
+
+		@app.route('/<redsocial>/<evento>/<numero>', methods=['GET', 'POST'])
+		def mapper(redsocial,evento,numero):
+			global acum
+			if numero:
+				#TODO: Con acum una estructura tipo Map
+				#Mapear la acumulacion por red social (y evento si se quiere)
+				#las variables son redsocial y evento
+				acum = acum + int(numero)
+			return 'true';
+		app.run()
+
+mapper 	= Mapper();
+rest 			= Rest();
+
 if __name__ == "__main__":
-    app.run()
+	#TODO: Hacer q termine la ejecucion, tipo con CTRL + C
+	rest.start()
+	mapper.start()
