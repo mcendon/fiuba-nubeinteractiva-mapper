f428d73d3d9766ba9600e83edf4d81973c6dfc5b
lt_caponpaul@hotmail.com <lt_caponpaul@hotmail.com@5af687ca-4c63-ab43-ae9e-5406cb19572c>
2013-10-12 21:40:07 +0000

    maneja 2 eventos distintos enviando a Controlador distinta respuesta por cada uno

--- mapper/mapper.py
+++ mapper/mapper.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+render_template, flash
 import threading, time, signal, sys, random
 
 #Controlador
@@ -7,35 +7,63 @@ import threading, time, signal, sys, random
 class Controlador():
 	def isIdle(self):
 		return bool(random.randint(0,1))
-		
+	
 	def hacerEfecto(self,idEfecto):
 		print("[Mensaje del Controlador]: Despache un efecto a la nube")
-		
+                
 #TODO: Cambiar esto por un map para contar notificaciones x red social
 #Onda {''facebook': 5, 'twitter': 10}
-acum = 0  #acumulacion
+
+filtroEventos = {'like':0, 'comment':0}  # acumulacion de eventos segun su tipo
+
+def inicializarTablaEventos():
+	
+	return {'like':1 , 'comment':2, 'likecomment':3}
 
 
 #Mapper
 #Thread que esta corriendo continuamente y mapea acciones al Controlador
-class Mapper(threading.Thread):  
-	def __init__(self):  
-		threading.Thread.__init__(self)  
+class Mapper(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
 		self.controlador = Controlador()
 
 	def run(self):
-		global acum
+		
+		global filtroEventos
+		
+		tablaEventos = inicializarTablaEventos()
+		cont = 0 # trabaja como iterador de la lista
+		
 		while(True):
-			if acum > 0:
-				print "[Mensaje del Mapper]: Tengo ", acum, " notificaciones para procesar"
-				if not self.controlador.isIdle():
+			
+			if len(filtroEventos) > 0:  # ver que poner, siempre es True
+				
+				listaEventos = filtroEventos.items() # lista DE TUPLAS con (clave, valor)
+				
+				print "[Mensaje del Mapper]: Tengo ", len(listaEventos), " notificaciones para procesar"
+				
+				if self.controlador.isIdle():
 					print "[Mensaje del Mapper]: El controlador ESTA disponible :)"
-					acum = acum - 1
-					self.controlador.hacerEfecto(400)
+							
+					evento = listaEventos[cont][0] #evento es el nombre del evento a enviar
+					efectoAEnviar = tablaEventos[evento] #efectoAEnviar es el NUMERO designado para el evento
+					self.controlador.hacerEfecto(efectoAEnviar)
+					
+					filtroEventos[evento] = 0 # reinicio contador de evento enviado
+					
+					cont += 1
+					
+					#para evitar ingresos invalidos reinicio contador
+					if cont >= len(listaEventos):
+						cont = 0
+					
 				else:
 					print "[Mensaje del Mapper]: El controlador no esta disponible :("
+					
 			else:
 				print "[Mensaje del Mapper]: Durmiendo... zzzzzZzzZzz"
+					
 			time.sleep(1)
 
 #Rest
@@ -45,7 +73,7 @@ class Mapper(threading.Thread):
 class Rest(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
-		
+	
 	def run(self):
 		app = Flask(__name__)
 		
@@ -58,20 +86,29 @@ class Rest(threading.Thread):
 		def index():
 			url_for('static', filename='nube_mapper.jpg');
 			return render_template('index.html');
-
+			
 		@app.route('/<redsocial>/<evento>/<numero>', methods=['GET', 'POST'])
 		def mapper(redsocial,evento,numero):
-			global acum
-			if numero:
-				#TODO: Con acum una estructura tipo Map
-				#Mapear la acumulacion por red social (y evento si se quiere)
-				#las variables son redsocial y evento
-				acum = acum + int(numero)
+			global filtroEventos
+			
+			cant = int(numero) # por las dudas =)
+			
+			if filtroEventos.has_key(evento.lower()):
+				
+				
+				#incremento la cant que tenia segun el num recibido
+				filtroEventos[evento.lower()] = filtroEventos[evento.lower()] + cant
+			
+			else:
+				#creo nuevo evento
+				filtroEventos[evento.lower()] = cant
+						
 			return 'true';
-		app.run()
+			
+			app.run()
 
-mapper 	= Mapper();
-rest 			= Rest();
+mapper  = Mapper();
+rest    = Rest();
 
 if __name__ == "__main__":
 	#TODO: Hacer q termine la ejecucion, tipo con CTRL + C
